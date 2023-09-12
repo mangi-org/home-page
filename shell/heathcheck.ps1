b7c24cba4d9b434bf62f05876d9173cedc4c5bc0
Alexsandr <alssval@gmail.com>
2023-09-13 00:48:32 +0300

    v0.0.3

--- shell/heathcheck.ps1
+++ shell/heathcheck.ps1
@@ -1,3 +1,3 @@
-vegeta attack -targets="healthcheck.txt" -duration=30s -rate=750/s -output="healthcheck.gob"
+vegeta attack -targets="healthcheck.txt" -duration=30s -rate=100/s -output="healthcheck.gob"
 vegeta report "healthcheck.gob"
 vegeta plot "healthcheck.gob" > plot.html
