3a397a94f4643dac600ea1e3940b5214aadd0f7a
Ilnur4ik <ilnu.2002@mail.ru>
2023-09-13 23:40:59 +0300

    Upd paralax

--- components/Paralax/Paralax.jsx
+++ components/Paralax/Paralax.jsx
@@ -1,56 +1,54 @@
 'use client'
-import styles from './Paralax.module.css'
-import Image from "next/image"
-import CenterImage from "@/assets/webexample-main.png"
-import ImageFirst from "@/assets/webexample1.png"
-import ImageSecond from "@/assets/webexample2.png"
-import ImageThird from "@/assets/webexample3.png"
-import ImageFourth from "@/assets/webexample4.png"
-import ImageFifth from "@/assets/webexample5.png"
-import { useEffect, useRef } from 'react'
-// import gsap from 'gsap'
-// import { Power0 } from 'gsap-trial'
+import { useEffect, useRef } from 'react';
+import styles from './Paralax.module.css';
+import Image from "next/image";
+import CenterImage from "@/assets/webexample-main.png";
+import ImageFirst from "@/assets/webexample1.png";
+import ImageSecond from "@/assets/webexample2.png";
+import ImageThird from "@/assets/webexample3.png";
+import ImageFourth from "@/assets/webexample4.png";
+import ImageFifth from "@/assets/webexample5.png";
 
 function Paralax() {
-
-    const refBackground = useRef(null)
-    const refMain = useRef(null)
+    const refMain = useRef(null);
+    const refFirst = useRef(null);
+    const refSecond = useRef(null);
+    const refThird = useRef(null);
+    const refFourth = useRef(null);
+    const refFifth = useRef(null);
 
     useEffect(() => {
-        // const timeline = new gsap.timeline();
-        // const bgImages = refBackground.current.children;
-        // const fgImage = refMain.current;
-        // const scrollAnimation = () => {
-        //     timeline.to(bgImages, {
-        //         y: -window.scrollY * 0.1,
-        //         ease: "Power0.easeNone"
-        //     })
-        //         .to(fgImage, {
-        //             y: -window.scrollY * 0.05,
-        //             ease: "Power0.easeNone"
-        //         });
-        // }
-        // window.addEventListener('scroll', scrollAnimation);
+    const handleScroll = () => {
+        const scrollY = window.scrollY;
+        refMain.current.style.transform = `translateY(${Math.max(Math.min(scrollY * 0.03, 200), -200)}px)`;
+        refFirst.current.style.transform = `translateY(${Math.max(Math.min(scrollY * 0.15, 200), -200)}px)`;
+        refSecond.current.style.transform = `translateY(${Math.max(Math.min(scrollY * 0.345, 200), -200)}px)`;
+        refThird.current.style.transform = `translateY(${Math.max(Math.min(scrollY * 0.125, 200), -200)}px)`;
+        refFourth.current.style.transform = `translateY(${Math.max(Math.min(scrollY * 0.08, 200), -200)}px)`;
+        refFifth.current.style.transform = `translateY(${Math.max(Math.min(scrollY * 0.135, 200), -200)}px)`;
+    };
 
-        // return () => {
-        //     window.removeEventListener('scroll', scrollAnimation);
-        // }
+    window.addEventListener('scroll', handleScroll);
 
-    }, [])
+    return () => {
+        window.removeEventListener('scroll', handleScroll);
+    };
+}, []);
+
 
     return (
         <div className={styles.paralax}>
             <Image src={CenterImage} alt="background image" ref={refMain} />
 
-            <div className={styles.background} ref={refBackground}>
-                <Image src={ImageFirst} alt="background image" />
-                <Image src={ImageSecond} alt="background image" />
-                <Image src={ImageThird} alt="background image" />
-                <Image src={ImageFourth} alt="background image" />
-                <Image src={ImageFifth} alt="background image" />
+            <div className={styles.background}>
+                <Image src={ImageFirst} alt="background image" ref={refFirst} />
+                <Image src={ImageSecond} alt="background image" ref={refSecond} />
+                <Image src={ImageThird} alt="background image" ref={refThird} />
+                <Image src={ImageFourth} alt="background image" ref={refFourth} />
+                <Image src={ImageFifth} alt="background image" ref={refFifth} />
             </div>
         </div>
-    )
+    );
 }
 
-export default Paralax
+export default Paralax;
