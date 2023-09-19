05ea7cccf610ea0ece0a12b94b60a78e96b89f52
Alexsandr <alssval@gmail.com>
2023-09-20 00:17:28 +0300

    update(about): update adaptation with and height

--- components/Sections/AboutUs/AboutUs.tsx
+++ components/Sections/AboutUs/AboutUs.tsx
@@ -1,5 +1,4 @@
 import { GradientSpan, Hydrogen, Paralax, Paragraph } from "../../UI/intex"
-import styles from "./AboutUs.module.css"
 
 
 /**
@@ -9,8 +8,8 @@ import styles from "./AboutUs.module.css"
  */
 function AboutUs() {
     return (
-        <section id="about" className={styles.section}>
-            <div className="content">
+        <section id="about" className="container mx-auto mt-4 relative">
+            <div className="flex flex-col justify-center text-center items-center">
                 <Hydrogen text="О нас" />
                 <Paragraph>
                     Команда Mangi.org находится вот уже <GradientSpan>7 лет на рынке</GradientSpan> и служит верностью и правдой своим клиентам
--- components/UI/Paralax/Paralax.tsx
+++ components/UI/Paralax/Paralax.tsx
@@ -1,6 +1,5 @@
 'use client'
-import { useEffect, useRef } from 'react';
-import styles from './Paralax.module.css';
+// import { useRef } from 'react';
 import Image from "next/image";
 import CenterImage from "@/assets/webexample-main.png";
 import ImageFirst from "@/assets/webexample1.png";
@@ -9,8 +8,6 @@ import ImageThird from "@/assets/webexample3.png";
 import ImageFourth from "@/assets/webexample4.png";
 import ImageFifth from "@/assets/webexample5.png";
 
-const scrollY = typeof window !== 'undefined' ? window.scrollY : 0;
-
 
 const settingsBackgroundImage = [
     {
@@ -18,28 +15,32 @@ const settingsBackgroundImage = [
         alt: 'background image',
         transform: {
             coefficient: 0.15
-        }
+        },
+        className: "w-1/3 left-1 top-6 -z-10 md:top-12",
     },
     {
         src: ImageSecond,
         alt: 'background image',
         transform: {
             coefficient: 0.345
-        }
+        },
+        className: "w-1/3 right-6 top-0 md:w-1/4 md:right-16",
     },
     {
         src: ImageThird,
         alt: 'background image',
         transform: {
             coefficient: 0.145
-        }
+        },
+        className: "w-1/3 right-1 top-32 md:top-60 lg:top-96",
     },
     {
         src: ImageFourth,
         alt: 'background image',
         transform: {
             coefficient: 0.08
-        }
+        },
+        className: "w-1/3 top-20 left-6 md:top-44 md:left-16 lg:top-60 lg:left-24 xl:top-72",
 
     },
     {
@@ -47,7 +48,8 @@ const settingsBackgroundImage = [
         alt: 'background image',
         transform: {
             coefficient: 0.135
-        }
+        },
+        className: "w-1/3 w-1/4 left-16 top-0 md:left-32 lg:left-48 xl:left-64",
     }
 ]
 
@@ -57,36 +59,52 @@ const settingsBackgroundImage = [
  * @return {JSX.Element} The JSX element representing the parallax effect.
  */
 function Paralax() {
-    const refMain = useRef<HTMLImageElement>(null);
-    const refBackImage = useRef<HTMLDivElement>(null);
+    // const refBackImage = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (!refMain.current) return;
-            if (!refBackImage.current) return;
-            const scrollY = window.scrollY;
-            refMain.current.style.transform = `translateY(${Math.max(Math.min(scrollY * 0.03, 200), -200)}px)`;
-            Array.from(refBackImage.current.children).forEach((item, index) => {
-                (item as HTMLImageElement).style.transform = `translateY(${Math.max(Math.min(scrollY * settingsBackgroundImage[index].transform.coefficient, 200), -200)}px)`;
-            })
+    // const [timelines, setTimelines] = useState<TimelineLite[]>([]);
 
-        };
+    // useEffect(() => {
+    //     if (!refBackImage.current) return;
 
-        window.addEventListener('scroll', handleScroll);
+    //     for (let item of refBackImage.current.children as HTMLCollection) {
+    //         const newTimeline = new TimelineLite();
+    //         newTimeline.from(item, 1.5, {
+    //             y: "-20%",
+    //             ease: Power2.easeInOut
+    //         }, "+=0.5");
+    //         setTimelines((prev) => [...prev, newTimeline]);
+    //     }
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+    //     console.log(timelines);
+    //     const handleScroll = () => {
+
+    //         const srollTop = window.pageYOffset;
+
+    //         timelines.forEach((item, index) => {
+    //             item.seek(srollTop);
+    //         })
+
+    //         // paralaxTimeline.seek(srollTop);
+    //     };
+
+    //     timelines.forEach((item, index) => {
+    //         item.play();
+    //     })
+    //     window.addEventListener('scroll', handleScroll);
+
+    //     return () => {
+    //         window.removeEventListener('scroll', handleScroll);
+    //     };
+    // }, [refBackImage.current]);
 
 
     return (
-        <div className={styles.paralax}>
-            <Image src={CenterImage} alt="background image" ref={refMain} />
+        <div className="relative w-full h-full flex items-center justify-center">
+            <Image src={CenterImage} className="shadow-2xl rounded w-1/2 my-12 z-10 md:my-20 lg:my-32" alt="background image" />
 
-            <div className={styles.background} ref={refBackImage}>
+            <div className='absolute top-0 left-0 w-full'>
                 {settingsBackgroundImage.map((item, index) => (
-                    <Image key={index} src={item.src} alt={item.alt}  />
+                    <Image className={"absolute shadow rounded  " + item.className} key={index} src={item.src} alt={item.alt} />
                 ))}
             </div>
         </div>
